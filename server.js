f11096ae062f7c788e4fb12306e0b35bda3046e9
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-11-04 08:15:38 -0300

    Organização dos comentários do arquivo server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 import express from "express";
 // Após instalação, adicionar o 'type: module' no arquivo package.json
-// Importando as rotas públicas
+
+// Importando as rotas públicas e privadas
 import publicRoutes from "./routes/public.js";
-// Importando as rotas privadas
 import privateRoutes from "./routes/private.js";
-//
+
+// Permissão para o front-end acessar o servidor
 import cors from "cors";
 
+// Importando o arquivo auth.js
 import auth from "./middlewares/auth.js";
 
 // Criação da variável app para englobar o express()
 const app = express();
+
 // Fazendo que que o express retorne em json (insonia)
 app.use(express.json());
+
 app.use(cors()); //Sites que poderão acessa o backend
 // app.use(cors("www.meusite.com.br")) -> Ambiente real
 
